3f25c905cf6e857cbe123978dadead6d67ceaca6
anushkadeshpande <anushkaa5000@gmail.com>
2024-01-25 23:58:21 +0530

    Added some explanation for the code

--- closures/app.js
+++ closures/app.js
@@ -17,3 +17,7 @@ console.log(z)
 
 z()       // this will print 7
 
+// Whenever functions are returned from another function, they still remember their lexical scope
+// hence even though x was done executing and y was returned, and later executed, it correctly printed out the value of a
+
+// When y was returned, actually the function was not returned, a closure was returned (fn + lexical scope)
